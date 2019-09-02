488be6a21bdc4fd1a8c8ee9c70bbe93d33656383
Guillerbr <ghminvest@gmail.com>
2019-09-02 11:49:36 -0300

    Add authController

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-app.get('/', (req, res) => {
-    res.send('OK');
-});
+require('./controllers/authController')(app);
 
 app.listen(3000);
 
