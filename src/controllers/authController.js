2be3d5e33c8039ae9851a32dd01205a7df11485a
Guillerbr <ghminvest@gmail.com>
2019-09-02 13:22:16 -0300

    Implement Jason web token -jws

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require ('jsonwebtoken');
 const router = express.Router();
+
+
+const authConfig = require('../config/auth.json');
 
 
 router.post('/register', async (req, res) => {
@@ -35,8 +39,17 @@ router.post('/authuser', async (req, res) => {
             if (!await bcrypt.compare(password, user.password)) 
             return res.status(400).send({ error: 'Invalid password' });
 
-            res.send({ user });
+            user.password = undefined;
+
+            const token = jwt.sign({ id: user.id}, authConfig.secret,{
+ 
+                   expiresIn: 86400,  //one day
+            });
+
+            res.send({ user, token });
 
 
 });
+
+
 module.exports = app => app.use('/auth', router);
