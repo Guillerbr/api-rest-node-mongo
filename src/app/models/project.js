e851c9c7467e6728678b310de771a19087e0d3ab
Guillerbr <ghminvest@gmail.com>
2019-09-03 19:06:08 -0300

    add news schema models db

---
+++ src/app/models/project.js
@@ -0,0 +1,38 @@
+const mongoose = require('../../database');
+const bcrypt = require('bcryptjs');
+
+const ProjectSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,     //referencia user id, comando moogoose para chave primaria
+        ref: 'User',
+        require: true,
+
+    },
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,   //referencia user id, comando moogoose para chave primaria
+        ref: 'Task',
+    }],
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+});
+
+
+
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+module.exports = Project;
