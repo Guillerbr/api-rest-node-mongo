e5c977b20445fefa9d3d1ee2656d8f71a654a928
Guillerbr <ghminvest@gmail.com>
2019-09-04 10:24:48 -0300

    add put operation controller

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -9,7 +9,7 @@ router.use(authMiddleware);
 
 router.get('/', async (req, res) => {
     try {
-        const projects = await Project.find().populate('user');
+        const projects = await Project.find().populate(['user', 'tasks']);
 
         return res.send({ projects });
 
@@ -38,13 +38,17 @@ router.post('/', async (req, res) => {
 
         const project = await Project.create({ title, description, user: req.userId });
 
-        tasks.map(task => {
+        await Promise.all(tasks.map(async task => {
 
             const projectTask = new Task({ ...task, project: project._id });
-            projectTask.save().then(task => project.tasks.push(task));
+
+            await projectTask.save();
+            project.tasks.push(projectTask);
 
 
-        });
+        }));
+
+        await project.save();
 
         return res.send({ project });
 
@@ -56,7 +60,35 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:projectId', async (req, res) => {
-    res.send({ user: req.userId });
+    try {
+        const { title, description, tasks } = req.body;
+
+        const project = await Project.findByIdAndUpdate(req.params.projectId, {
+            title,
+            description
+            
+        },{ new: true });
+
+        project.tasks =[];
+        await task.remove({ project: project._id })
+
+        await Promise.all(tasks.map(async task => {
+
+            const projectTask = new Task({ ...task, project: project._id });
+
+            await projectTask.save();
+            project.tasks.push(projectTask);
+
+
+        }));
+
+        await project.save();
+
+        return res.send({ project });
+
+    } catch (err) {
+        return res.status(400).send({ error: "Error updating project!" });
+    }
 
 });
 
