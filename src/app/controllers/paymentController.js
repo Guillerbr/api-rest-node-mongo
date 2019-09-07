776d81d13a6cb56b975a7d8811ddb904a4a84883
Guillerbr <ghminvest@gmail.com>
2019-09-07 13:15:08 -0300

    add router payments get

--- src/app/controllers/paymentController.js
+++ src/app/controllers/paymentController.js
@@ -4,3 +4,16 @@ const router = express.Router()
 
 const Project = require('../models/project');
 
+router.use(authMiddleware);
+
+router.get('/payments', async (req, res) => {
+    try {
+        const projects = await Project.find().populate(['user', 'tasks']);
+
+        return res.send({ projects });
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Error loading projects!' });
+
+    }
+});
