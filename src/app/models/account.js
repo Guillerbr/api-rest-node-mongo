444a0e8b8d60a45c7a9b2849c9da4251675f27ee
Guillerbr <ghminvest@gmail.com>
2019-09-23 19:43:15 -0300

    add schema atributes account

--- src/app/models/account.js
+++ src/app/models/account.js
@@ -2,6 +2,40 @@ const mongoose = require('../../database');
 
 const AccountSchema = new mongoose.Schema({
 
+    user: {
+        type: mongoose.Schema.Types.ObjectId,     //referencia user id, comando moogoose para chave primaria
+        ref: 'User',                             //relacionamento de tabelas
+        require: true,
+
+    },
+    id_account: {
+        type: Number,
+        required: true
+        //select: false,
+    },
+    balance: {
+        type: Number,
+        required: true
+        //select: false,
+    },
+    deposits: {
+        type: Number,
+        required: true
+        //select: false,
+    },
+    shopping: {
+        type: Number,
+        required: true
+        //select: false,
+    },
+
+
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
 
 
 
