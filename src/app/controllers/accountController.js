f94cb77d2b3daa3b0bdb3bb77ffc27968e9eca39
Guillerbr <ghminvest@gmail.com>
2019-09-23 19:45:53 -0300

    create router post account controller

--- src/app/controllers/accountController.js
+++ src/app/controllers/accountController.js
@@ -6,3 +6,24 @@ const Account = require('../models/account');
 
 
 router.use(authMiddleware);
+
+
+router.post('/account', async (req, res) => {
+
+
+    try {
+        const { id_account, balance, deposits, shopping } = req.body;
+
+        const account = await Account.create({ id_account, balance, deposits, shopping, user: req.userId });
+        await account.save();
+
+        return res.send({ account });
+
+
+    } catch (err) {
+        return res.status(400).send({
+            error: 'Error create account!'
+        });
+
+    }
+});
